7fdb5cb899092c56f20a034000b02d9ce4d4cff6
Jatin <jatintomar268@gmail.com>
2017-07-18 00:33:05 +0530

    Commit till  service

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -7,12 +7,17 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-
+            $scope.showMenu = false;
+            $scope.message = "Loading...";
             $scope.dishes= {};
             menuFactory.getDishes()
             .then(
                 function(response) {
                     $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status +" "+ response.statusText;
                 }
             );
             
@@ -78,11 +83,16 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
             
             $scope.dish = {};
+            $scope.showDish = false;
+            $scope.message="Loading...";
             menuFactory.getDish(parseInt($stateParams.id,10))
             .then(
                 function(response){
                     $scope.dish = response.data;
                     $scope.showDish=true;
+                },
+                function(response){
+                    $scope.message = "Error: "+response.status +" " + response.statusText;
                 }
             );
             
@@ -110,11 +120,16 @@ angular.module('confusionApp')
            
 
             $scope.featuredDish = {};
+            $scope.showDish = false;
+            $scope.message="Loading...";
             menuFactory.getDish(0)
             .then(
                 function(response){
                     $scope.featuredDish = response.data;
                     $scope.showDish = true;
+                },
+                function(response){
+                    $scope.message = "Error: " +response.status + " " + response.statusText;
                 }
             );
             
